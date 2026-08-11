66996cbe5294d1286a48ccb40ef2f989cc2f96fb
jlefx <jlee@serenetics.com>
2026-08-10 18:03:18 -0600

    Added explicit thinking and effort values. In DeepSeek, the default is thinking and high.

--- src/agent_week1/__init__.py
+++ src/agent_week1/__init__.py
@@ -1,6 +1,10 @@
 import anthropic
-from anthropic.types import MessageParam
 import os
+from anthropic.types import (
+    MessageParam,
+    ThinkingConfigEnabledParam,
+    OutputConfigParam,
+)
 
 
 def main() -> None:
@@ -9,7 +13,7 @@ def main() -> None:
     # Initialize the client (pointing to DeepSeek's Anthropic-compatible endpoint)
     client = anthropic.Anthropic(
         base_url=os.environ["ANTHROPIC_BASE_URL"],
-        api_key=os.environ["ANTHROPIC_API_KEY"] # It's recommended to read this from an environment variable
+        api_key=os.environ["ANTHROPIC_API_KEY"]  # It's recommended to read this from an environment variable
     )
 
     # Send a message request
@@ -20,11 +24,22 @@ def main() -> None:
         }
     ]
 
+    thinking_config: ThinkingConfigEnabledParam = {
+        "type": "enabled",
+        "budget_tokens": 1024,
+    }
+
+    output_config: OutputConfigParam = {
+        "effort": "high",
+    }
+
     with client.messages.stream(
             model="deepseek-v4-pro",
-            max_tokens=1000,
+            max_tokens=2000,
             system="Your name is Serenity. You are a helpful assistant.",
             messages=messages,
+            thinking=thinking_config,
+            output_config=output_config,
     ) as stream:
         for text in stream.text_stream:
             print(text, end="", flush=True)
